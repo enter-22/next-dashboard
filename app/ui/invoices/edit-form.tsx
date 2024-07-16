26ba596c45119eb8a0f496541ca9a78e85693202
enter-22 <zuka.yuuki@gmail.com>
2024-07-16 10:02:33 +0900

    update

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
   CheckIcon,
@@ -9,7 +8,10 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-
+import { updateInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
+import ErrorMsg from '@/app/ui/invoices/error-msg';
+import { UpdateInvoice } from './buttons';
 export default function EditInvoiceForm({
   invoice,
   customers,
@@ -17,8 +19,11 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(updateInvoice, initialState);
   return (
-    <form>
+    <form action={dispatch}>
+      <input type='hidden' name='id' value={invoice.id} />
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -31,6 +36,7 @@ export default function EditInvoiceForm({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -43,6 +49,7 @@ export default function EditInvoiceForm({
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+          <ErrorMsg id="customer-error" errors={state.errors?.customerId} />
         </div>
 
         {/* Invoice Amount */}
@@ -59,10 +66,12 @@ export default function EditInvoiceForm({
                 step="0.01"
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
+                aria-describedby="customer-error"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+            <ErrorMsg id="customer-error" errors={state.errors?.amount} />
           </div>
         </div>
 
@@ -80,6 +89,7 @@ export default function EditInvoiceForm({
                   type="radio"
                   value="pending"
                   defaultChecked={invoice.status === 'pending'}
+                  aria-describedby="customer-error"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
@@ -96,6 +106,7 @@ export default function EditInvoiceForm({
                   type="radio"
                   value="paid"
                   defaultChecked={invoice.status === 'paid'}
+                  aria-describedby="customer-error"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
@@ -109,6 +120,7 @@ export default function EditInvoiceForm({
           </div>
         </fieldset>
       </div>
+      <ErrorMsg id="customer-error" errors={state.errors?.status} />
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/invoices"
